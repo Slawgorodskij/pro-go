150ff94772d1af83f8b1b0462ffb965ca7f6b3e9
vladimir <vladimir.slavgorodskiy@intelsib-team.com>
2023-09-22 22:28:02 +0700

    глава 14

---
+++ concurrency/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("main function started")
+	//CalcStoreTotal(Products)
+	//time.Sleep(time.Second * 5)
+	//fmt.Println("main function complete")
+
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrder(dispatchChannel)
+	//for {
+	//	if details, open := <-dispatchChannel; open {
+	//		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+	//	} else {
+	//		fmt.Println("Channel has been closed")
+	//		break
+	//	}
+	//
+	//}
+	for details := range dispatchChannel {
+		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+	}
+	fmt.Println("Channel has been closed")
+}
