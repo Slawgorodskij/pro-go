d943f898edb9f491aa20782fe51e9004f3161fc4
vladimir <vladimir.slavgorodskiy@intelsib-team.com>
2024-02-09 11:02:34 +0700

    глава 20

--- readerandwrites/main.go
+++ readerandwrites/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"io"
+	"strings"
+)
+
 /*кодирование данных JSON*/
 
 // func main() {
@@ -283,3 +289,163 @@ Decoded Integer: 200
 */
 
 /*Указание типов для декодирования*/
+
+// func main() {
+// 	reader := strings.NewReader(`true "Hello" 99.99 200`)
+
+// 	var bval bool
+// 	var sval string
+// 	var fpval float64
+// 	var ival int
+
+// 	vals := []interface{}{&bval, &sval, &fpval, &ival}
+
+// 	decoder := json.NewDecoder(reader)
+
+// 	for i := 0; i < len(vals); i++ {
+// 		err := decoder.Decode(vals[i])
+// 		if err != nil {
+
+// 			Printfln("Error: %v", err.Error())
+
+// 			break
+// 		}
+// 	}
+
+// 	Printfln("Decoded (%T): %v", bval, bval)
+// 	Printfln("Decoded (%T): %v", sval, sval)
+// 	Printfln("Decoded (%T): %v", fpval, fpval)
+// 	Printfln("Decoded (%T): %v", ival, ival)
+// }
+
+/*
+Decoded (bool): true
+Decoded (string): Hello
+Decoded (float64): 99.99
+Decoded (int): 200
+*/
+
+/* Декодирование массивов*/
+
+// func main() {
+// 	reader := strings.NewReader(`[10, 20, 30]["Kayak", "Lifejacket",279]`)
+// 	vals := []interface{}{}
+
+// 	decoder := json.NewDecoder(reader)
+
+// 	for {
+// 		var decoderVal interface{}
+// 		err := decoder.Decode(&decoderVal)
+// 		if err != nil {
+// 			if err != io.EOF {
+// 				Printfln("Error: %v", err.Error())
+// 			}
+// 			break
+// 		}
+// 		vals = append(vals, decoderVal)
+// 	}
+
+// 	for _, val := range vals {
+// 		Printfln("Decoded (%T): %v", val, val)
+// 	}
+// }
+
+/*
+Decoded ([]interface {}): [10 20 30]
+Decoded ([]interface {}): [Kayak Lifejacket 279]
+*/
+
+/*Если заранее известна структура данных JSON*/
+
+// func main() {
+// 	reader := strings.NewReader(`[10, 20, 30]["Kayak", "Lifejacket",279]`)
+
+// 	ints := []int{}
+// 	mixed := []interface{}{}
+
+// 	vals := []interface{}{&ints, &mixed}
+
+// 	decoder := json.NewDecoder(reader)
+
+// 	for i := 0; i < len(vals); i++ {
+// 		err := decoder.Decode(vals[i])
+// 		if err != nil {
+// 			Printfln("Error: %v", err.Error())
+// 			break
+// 		}
+// 	}
+
+// 	Printfln("Decoded (%T): %v", ints, ints)
+// 	Printfln("Decoded (%T): %v", mixed, mixed)
+// }
+
+/*
+Decoded ([]int): [10 20 30]
+Decoded ([]interface {}): [Kayak Lifejacket 279]
+*/
+
+/*Декодирование карт*/
+
+// func main() {
+// 	reader := strings.NewReader(`{"Kayak" : 279, "Lifejacket" : 49.95}`)
+
+// 	//m := map[string]interface{}{}
+// 	m := map[string]float64{}
+
+// 	decoder := json.NewDecoder(reader)
+
+// 	err := decoder.Decode(&m)
+
+// 	if err != nil {
+// 		Printfln("Error: %v", err.Error())
+// 	} else {
+// 		Printfln("Map %T, %v", m, m)
+
+// 		for k, v := range m {
+// 			Printfln("Key: %v, Value: %v", k, v)
+// 		}
+// 	}
+// }
+/*
+m := map[string]interface{}{}
+Map map[string]interface {}, map[Kayak:279 Lifejacket:49.95]
+Key: Kayak, Value: 279
+Key: Lifejacket, Value: 49.95
+
+m := map[string]float64{}
+Map map[string]float64, map[Kayak:279 Lifejacket:49.95]
+Key: Kayak, Value: 279
+Key: Lifejacket, Value: 49.95
+*/
+
+/*Декодирование структур*/
+
+func main() {
+	reader := strings.NewReader(`
+	{"Name" : "Kayak", "Category": "Watersports", "Price" : 279}
+	{"Name" : "Lifejacket", "Category": "Watersports"}
+	{"Name" : "Canoe", "Category": "Watersports", "price" : 100, "InStock" : true}
+	`)
+
+	decoder := json.NewDecoder(reader)
+
+	for {
+		var val Product
+		err := decoder.Decode(&val)
+		if err != nil {
+			if err != io.EOF {
+				Printfln("Error: %v", err.Error())
+			}
+			break
+		} else {
+			Printfln("Name: %v, Category: %v, Price: %v", val.Name, val.Category, val.Price)
+		}
+
+	}
+}
+
+/*
+Name: Kayak, Category: Watersports, Price: 279
+Name: Lifejacket, Category: Watersports, Price: 0
+Name: Canoe, Category: Watersports, Price: 100
+*/
