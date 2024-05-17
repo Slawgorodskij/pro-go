85631e8d18c28d3435a1d0501e4e99b203c095d5
vladimir <vladimir.slavgorodskiy@intelsib-team.com>
2024-05-17 19:13:53 +0700

    глава 26

---
+++ data/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "database/sql"
+
+func queryDatabase(db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM Products")
+	if err == nil {
+		for rows.Next() {
+			var id, category int
+			var name string
+			var price float64
+			rows.Scan(&id, &name, &category, &price)
+			Printfln("Row: %v %v %v %v", id, name, category, price)
+		}
+	} else {
+		Printfln("Error: %v", err)
+	}
+}
+
+func main() {
+
+	db, err := openDatabase()
+	if err == nil {
+		queryDatabase(db)
+		db.Close()
+	} else {
+		panic(err)
+	}
+}
