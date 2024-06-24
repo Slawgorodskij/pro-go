a3ffe17b9eea37d56c8990edad43c1f3707fa39f
vladimir <vladimir.slavgorodskiy@intelsib-team.com>
2024-06-24 16:05:51 +0700

    глава 32

--- platform/main.go
+++ platform/main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"platform/config"
 	"platform/logging"
+	"platform/services"
 )
 
-func writeMessage(logger logging.Logger) {
-	logger.Info("Hello, Platform")
+func writeMessage(logger logging.Logger, cfg config.Configuration) {
+	section, ok := cfg.GetSection("main")
+	if ok {
+		message, ok := section.GetString("message")
+		if ok {
+			logger.Info(message)
+		} else {
+			logger.Panic("Cannot find configuration setting")
+		}
+	} else {
+		logger.Panic("Config section not found")
+	}
 }
+
 func main() {
-	var logger logging.Logger = logging.NewDefaultLogger(logging.Information)
-	writeMessage(logger)
+	services.RegisterDefaultServices()
+	/*
+		Становится не нужным после создания Call
+			var cfg config.Configuration
+			services.GetService(&cfg)
+
+			var logger logging.Logger
+			services.GetService(&logger)
+
+			writeMessage(logger, cfg)
+	*/
+	services.Call(writeMessage)
+
+	val := struct {
+		message string
+		logging.Logger
+	}{
+		message: "Hello from the struct",
+	}
+	services.Populate(&val)
+	val.Logger.Debug(val.message)
 }
