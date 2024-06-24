424a8fe4149d1f12e110e74e526e3903dbb4284b
vladimir <vladimir.slavgorodskiy@intelsib-team.com>
2024-06-24 16:40:05 +0700

    глава 33

---
+++ platform/pipeline/component.go
@@ -0,0 +1,22 @@
+package pipeline
+
+import "net/http"
+
+type ComponentContext struct {
+	*http.Request
+	http.ResponseWriter
+	error
+}
+
+func (mws *ComponentContext) Error(err error) {
+	mws.error = err
+}
+
+func (mws *ComponentContext) GetError() error {
+	return mws.error
+}
+
+type MiddlewareComponent interface {
+	Init()
+	ProcessRequest(ctx *ComponentContext, next func(*ComponentContext))
+}
